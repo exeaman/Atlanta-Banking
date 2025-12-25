2b99d97e8fcb07c17c292bbdeef31f124425becb
Aman Jaiswal <dynamite8931p@gmail.com>
2025-12-25 14:55:15 +0530

    Added the Folders and Related Dependencies

---
+++ statement-service/src/test/java/com/atlanta/banking/statement/service/StatementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atlanta.banking.statement.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
